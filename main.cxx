4cec4c24463670087cc100c8ecb0eb284d173256
guysa-AMG <moosathejoker@gmail.com>
2026-08-09 06:13:34 +0200

    Refactor code structure for improved readability and maintainability

--- main.cxx
+++ main.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "toml++/toml.hpp"
 #include <string>
 using namespace std;
@@ -25,8 +26,7 @@ std::vector<User> parsefile(std::string path){
     
     for(auto&& [key,value]: tbl){
        if(toml::table* info = value.as_table()) {
-       User* user= new User (key.str(),(*info)["user"].as_string(),(*info)["email"].as_string());
-          list.push_back(*user);
+          list.emplace_back(key.str(), (*info)["user"].as_string(), (*info)["email"].as_string());
        };
         
     }
@@ -38,15 +38,16 @@ int main (int argc,char ** argv){
     
     std::vector<User> result = parsefile(".userlist/profiles.toml");
     int indx =0;
-    if (argc==0){
-        int counter=1;
-        for(auto user:  result){
-            cout<<"["<<counter<<"] "<< result[counter-1].name<<endl;
+    if (argc==1){
+        
+        for(int ix=0; ix<result.size();ix++){
+            cout<<"["<<ix+1<<"] "<< result[ix].name<<endl;
+      
         }
         int choice;
-        cin>>choice;
+       cin>>choice;
    
-       if(choice>0 && choice<result.size())
+       if(choice>0 && choice<= static_cast<int>(result.size()))
         {
             indx = choice-1;
         }
@@ -54,6 +55,7 @@ int main (int argc,char ** argv){
             cerr<<"invalid choice"<<endl;
         }
     }else{
+
         indx=atoi(argv[1]);
     }
     std::cout<<"\033[92m ... switching into \033[00m"<<result[indx].name <<std::endl;
--- makefile
+++ makefile
@@ -1,3 +1,9 @@
-app ?="autcher"
+app ?="authchanger"
+installPath ?= "/usr/bin"
 build:
 	g++  main.cxx -o ${app} -I toml++/
+
+install: build
+	cp ${app} ${installPath}
+	cp -r .userlist/ ${installPath}
+
